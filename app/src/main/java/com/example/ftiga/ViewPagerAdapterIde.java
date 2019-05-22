33e90397ccba9ab7855eac2dbd4b28fbb4ff3f72
adhi98 <30853092+adhi98@users.noreply.github.com>
2019-05-22 15:09:46 +0700

    Detail Ide

---
+++ app/src/main/java/com/example/ftiga/ViewPagerAdapterIde.java
@@ -0,0 +1,41 @@
+package com.example.ftiga;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapterIde extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentListTitles = new ArrayList<>();
+
+    public ViewPagerAdapterIde(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return  fragmentListTitles.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentListTitles.get(position);
+    }
+
+    public void AddFragment(Fragment fragment, String Title){
+        fragmentList.add(fragment);
+        fragmentListTitles.add(Title);
+    }
+
+}
