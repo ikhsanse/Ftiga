b7a1d7af9117b2dc95b807b0e4c10730b0549884
hizkiamr1 <hizkiarobinsar@gmail.com>
2019-05-27 17:30:22 +0700

    updateide

---
+++ app/src/main/java/com/example/ftiga/form_ide4.java
@@ -0,0 +1,13 @@
+package com.example.ftiga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class form_ide4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_ide4);
+    }
+}
