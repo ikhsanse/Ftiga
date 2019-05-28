d98a21c3a07b2069b9148f33fb80ce504ff795a8
adhi98 <30853092+adhi98@users.noreply.github.com>
2019-05-28 22:38:11 +0700

    History Ide

---
+++ app/src/main/java/com/example/ftiga/FragmentHistoryDonasi.java
@@ -0,0 +1,69 @@
+package com.example.ftiga;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class FragmentHistoryDonasi extends Fragment {
+    private View view;
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> dataSet;
+
+    public FragmentHistoryDonasi() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_history_donasi, container,false);
+        dataSet = new ArrayList<>();
+        initDataset();
+
+        rvView = (RecyclerView) view.findViewById(R.id.rv_main);
+        rvView.setHasFixedSize(true);
+
+        /**
+         * Kita menggunakan LinearLayoutManager untuk list standar
+         * yang hanya berisi daftar item
+         * disusun dari atas ke bawah
+         */
+        layoutManager = new LinearLayoutManager(getActivity());
+        rvView.setLayoutManager(layoutManager);
+
+        adapter = new HistoryDonasiActivity(dataSet);
+        rvView.setAdapter(adapter);
+        return view;
+    }
+
+    private void initDataset(){
+
+        /**
+         * Tambahkan item ke dataset
+         * dalam prakteknya bisa bermacam2
+         * tidak hanya String seperti di kasus ini
+         */
+
+        dataSet.add("Karin");
+        dataSet.add("Ingrid");
+        dataSet.add("Helga");
+        dataSet.add("Renate");
+        dataSet.add("Elke");
+        dataSet.add("Ursula");
+        dataSet.add("Erika");
+        dataSet.add("Christa");
+        dataSet.add("Gisela");
+        dataSet.add("Monika");
+
+    }
+}
---
+++ app/src/main/java/com/example/ftiga/HistoryIdeActivity.java
@@ -0,0 +1,60 @@
+package com.example.ftiga;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class HistoryIdeActivity extends RecyclerView.Adapter<HistoryIdeActivity.ViewHolder> {
+
+    private ArrayList<String> rvData;
+
+
+    public HistoryIdeActivity(ArrayList<String> rvData) {
+        this.rvData = rvData;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        // di tutorial ini kita hanya menggunakan data String untuk tiap item
+        public TextView tv_judul_ide;
+        public TextView tv_jumlah_donasi;
+
+        public ViewHolder(View v) {
+            super(v);
+            tv_judul_ide = (TextView) v.findViewById(R.id.tv_judul_ide);
+            tv_jumlah_donasi = (TextView) v.findViewById(R.id.tv_jumlah_donasi);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // membuat view baru
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_history_ide, parent, false);
+        // mengeset ukuran view, margin, padding, dan parameter layout lainnya
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - mengambil elemen dari dataset (ArrayList) pada posisi tertentu
+        // - mengeset isi view dengan elemen dari dataset tersebut
+        final String name = rvData.get(position);
+        holder.tv_judul_ide.setText(rvData.get(position));
+        holder.tv_jumlah_donasi.setText("Rp" + position);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        // menghitung ukuran dataset / jumlah data yang ditampilkan di RecyclerView
+        return rvData.size();
+    }
+
+}
